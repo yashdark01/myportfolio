4fa59055b0da54710c0ab23d3873842374bcc695
Yash Patidar <137425534+yashdark01@users.noreply.github.com>
2025-02-07 17:18:17 +0530

    Update Projects.jsx

--- portfolio/src/sections/Projects.jsx
+++ portfolio/src/sections/Projects.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Element } from "react-scroll";
-import { FaGithub, FaExternalLinkAlt } from "react-icons/fa"; // Icons for GitHub and External links
+import { FaGithub, FaExternalLinkAlt } from "react-icons/fa";
 
 const projects = [
   {
@@ -35,15 +35,13 @@ const Projects = () => {
       <div className="max-w-6xl mx-auto px-6">
         <h2 className="text-5xl font-extrabold text-center mb-12 glow-text">My Projects</h2>
 
-        {/* Project Cards */}
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-12">
           {projects.map((project, index) => (
             <div key={index} className="bg-gray-800 p-6 rounded-lg shadow-lg hover:scale-105 transition-transform duration-300 relative group">
-              {/* Project Title and Short Description */}
+              
               <h3 className="text-2xl font-semibold">{project.title}</h3>
               <p className="text-gray-300 mt-4">{project.description}</p>
 
-              {/* Technologies Used */}
               <div className="flex flex-wrap gap-4 mt-4">
                 {project.technologies.map((tech, idx) => (
                   <span key={idx} className="bg-gray-700 text-cyan-500 hover:text-cyan-300 hover:scale-110 px-4 py-2 rounded-full text-sm">
@@ -52,7 +50,6 @@ const Projects = () => {
                 ))}
               </div>
 
-              {/* Read More / Less Button */}
               <button
                 onClick={() => toggleDescription(index)}
                 className="text-cyan-400 mt-4 text-lg font-semibold hover:text-cyan-500"
@@ -60,12 +57,10 @@ const Projects = () => {
                 {expandedIndex === index ? "Read Less" : "Read More"}
               </button>
 
-              {/* Full Description */}
               {expandedIndex === index && (
                 <p className="text-gray-400 mt-4">{project.fullDescription}</p>
               )}
 
-              {/* GitHub and Live Link Icons */}
               <div className="absolute bottom-6 right-6 flex space-x-4 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
                 <a href={project.githubLink} target="_blank" rel="noopener noreferrer">
                   <FaGithub size={30} className="text-cyan-400 hover:text-cyan-500 transition-colors" />
